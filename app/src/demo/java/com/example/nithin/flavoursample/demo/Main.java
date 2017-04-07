fcafe76fa110c4e4c48bf9b0b34af7d91be7cb36
Nithinpaul <nithpl@gmail.com>
2017-04-07 11:08:12 +0530

    Initial commit

---
+++ app/src/demo/java/com/example/nithin/flavoursample/demo/Main.java
@@ -0,0 +1,21 @@
+package com.example.nithin.flavoursample.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.nithin.flavoursample.R;
+
+/**
+ * Created by nithin on 1/12/2017.
+ */
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+    }
+
+}
---
+++ app/src/full/java/com/example/nithin/flavoursample/full/Login.java
@@ -0,0 +1,21 @@
+package com.example.nithin.flavoursample.full;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.nithin.flavoursample.R;
+
+/**
+ * Created by nithin on 1/11/2017.
+ */
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+    }
+
+}
